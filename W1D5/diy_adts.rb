52d5b9d04081d9ff2d615ded3f850f18275a3e41
Lisa Steele <lisahsteele@gmail.com>
2019-03-14 22:48:02 -0700

    exercise 3 Map complete

--- W1D5/diy_adts.rb
+++ W1D5/diy_adts.rb
@@ -1,3 +1,5 @@
+require "byebug"
+
 class Stack
   #LIFO
   def initialize(stack)
@@ -39,4 +41,45 @@ class Queue
 
 end
 
+class Map 
+  def initialize
+    @map = Array.new 
+    
+  end 
+
+  def set(key, value)
+    if @map.length == 0
+      @map << [key, value]
+    else
+      @map.each do |subarr|
+        if subarr[0] == key
+          subarr[1] = value
+          return
+        end
+      end
+      @map << [key, value]
+    end
+  end
+
+  def get(key)
+    @map.each do |subarr|
+      if subarr[0] == key 
+        print subarr
+      end
+    end
+  end
+
+  def delete(key)
+    @map.each.with_index do |subarr, idx|
+      if subarr[0] == key 
+        @map.delete_at(idx)
+      end
+    end
+  end
+
+  def show 
+    print @map
+  end
+
+end
 
