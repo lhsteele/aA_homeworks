831d2d3ea512b6b05a8431fd5d055f9024af6a4b
Lisa Steele <lisahsteele@gmail.com>
2019-05-08 21:34:39 -0700

    w9d3 and d4 homeworks

---
+++ w9d3/React_Job_Listing/frontend/reducer.js
@@ -0,0 +1,21 @@
+import { selectLocation } from './actions';
+
+const initialState = {
+  city: "Please Select", 
+  jobs: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case "SWITCH_LOCATION":
+      return {
+        city: action.city, 
+        jobs: action.jobs
+      };
+    default:
+      return state;
+  }
+} 
+
+export default reducer;
+
